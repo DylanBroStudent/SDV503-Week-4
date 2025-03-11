79c3567720d5052cb3b0e1514f5d454ad612c62d
Dylan <dylan-brodie@live.nmit.ac.nz>
2025-03-12 10:16:36 +1300

    second block of variable work

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+/*
+
 // const firstName = 'dylan'
 // const lastName = 'brodie'
 // Camel case 
@@ -26,5 +28,38 @@ console.log(gravity, boilingPoint, pi)
 
 // Variable can also be declared on a single line seperated by a comma
 
-let name = 'dylan', job = 'student', live = 'nelson'
+//let name = 'dylan', job = 'student', live = 'nelson'
 console.log(name, job, live)
+
+*/
+
+/*
+Excersize - 1 : Variables
+1 - Declare four variables without assigning values
+2 - Declare four variables with assigning values
+3 - Declare vaiables to store your first name, last name, student status, country, and age, in multiple lines
+4 - Declare two variables myAge and yourAge and assign them inital values and log them to browser console. 
+
+*/
+
+let var1
+let var2
+let var3
+let var4
+
+let varA = "A"
+let varB = "B"
+let varC = "C"
+let varD = "D"
+
+const firstName = 'Dylan'
+const lastName = 'Brodie'
+const studentStatus = 'Enrolled'
+const country = 'New Zealand'
+let age = 28
+
+let myAge = 28
+let yourAge = 76
+
+console.log('I am', myAge, 'years old')
+console.log('you are', yourAge, 'years old')
