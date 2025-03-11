7f220e36d2076c5ec8ac9da2ee00ca6040233c5b
Dylan <dylan-brodie@live.nmit.ac.nz>
2025-03-12 09:56:02 +1300

    variable testing

---
+++ App.js
@@ -0,0 +1,30 @@
+// const firstName = 'dylan'
+// const lastName = 'brodie'
+// Camel case 
+// const classCode = 'SDV503'
+
+// Invalid Variables
+// first-name
+// 1_num
+// num_#_1
+
+// Declaring different variables of different data types
+let firstName = 'Dylan'
+let lastName = 'Brodie'
+let country = 'New Zealand'
+let age = 28
+let isStudent = true
+
+console.log(firstName, lastName, country, age, isStudent)
+
+// Declaring Variables with number values
+
+const gravity = 9.81
+const boilingPoint = 100
+const pi = 3.141592653
+console.log(gravity, boilingPoint, pi)
+
+// Variable can also be declared on a single line seperated by a comma
+
+let name = 'dylan', job = 'student', live = 'nelson'
+console.log(name, job, live)
